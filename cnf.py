c615e1a6ffa9cc79450c7626b00145db94602235
Duong Manh <duongtienmanh8121995@gmail.com>
2020-12-01 18:19:53 +0000

    update

--- cnf.py
+++ cnf.py
@@ -1,14 +1,22 @@
 class Cnf:
   def __init__(self):
-    self.true = frozenset([frozenset([1])])
-    self.false = frozenset([frozenset([-1])])
-    self.data = self.true
+    self.data = frozenset([frozenset([1])])
   
+  def cnf_false(self):
+    cnf = Cnf()
+    cnf.data = frozenset([frozenset([-1])])
+    return cnf
+
+  def cnf_true(self):
+    cnf = Cnf()
+    cnf.data = frozenset([frozenset([1])])
+    return cnf
+
   def __and__(self, other):
     cnf = Cnf()
     # A & F = F
     if other.is_false():
-      return self.false
+      return self.cnf_false()
     
     # A & T = A
     if other.is_true():
@@ -22,24 +30,43 @@ class Cnf:
 
     # F & A = F
     if self.is_false():
-      return self.false
+      return self.cnf_false()
 
     # A & -A = F
     self_var = self.get_var()
     other_var = other.get_var()
     if self_var is not None and other_var is not None:
       if self_var == -other_var:
-        return self.false
+        return self.cnf_false()
 
     # A & A = A
-    cnf.data = self.data.union(self.data, other.data)
+    tmp = frozenset([])
+    cnf.data = tmp.union(self.data, other.data)
     return cnf
 
   def __or__(self, other):
-    pass
+    cnf = Cnf()
+    for self_u in self.data:
+      for other_u in other.data:
+        tmp = frozenset([])
+        # A | A = A
+        tmp = tmp.union(self_u, other_u)
+        # A | -A = T
+        for e in tmp:
+          if -e in tmp:
+            tmp = frozenset([1])
+        # A | T = T
+        if 1 in tmp:
+          tmp = frozenset([1])
+        # A | F = A
+        tmp.symmetric_difference(frozenset([-1]))  
+        cnftmp = Cnf()
+        cnftmp.data = frozenset([tmp])
+        cnf = (cnf & cnftmp)
+    return cnf
 
   def is_true(self):
-    return self.data == self.true
+    return self.data == frozenset([frozenset([1])])
   
   def get_var(self):
     if len(self.data) == 1:
@@ -50,7 +77,7 @@ class Cnf:
     return None
 
   def is_false(self):
-    return self.data == self.false
+    return self.data == frozenset([frozenset([-1])])
 
   def __str__(self):
     print(self.data)
@@ -58,8 +85,8 @@ class Cnf:
 
 if __name__ == "__main__":
   cnf1 = Cnf()
-  cnf1.data = frozenset([frozenset([-2])])
+  cnf1.data = frozenset([frozenset([1, 2, 3, -1]), frozenset([5,6])])
   cnf2 = Cnf()
-  cnf2.data = frozenset([frozenset([2])])
-  cnf3 = cnf1 & cnf2
+  cnf2.data = frozenset([frozenset([1]), frozenset([3])])
+  cnf3 = cnf1 | cnf2
   print(cnf3)
