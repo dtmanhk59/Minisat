c158d91faf73df40a22034dfac4510c4848530a0
Duong Manh <duongtienmanh8121995@gmail.com>
2020-12-03 17:14:28 +0000

    update

--- cnf.py
+++ cnf.py
@@ -2,27 +2,27 @@ import itertools
 
 class Var:
   def __init__(self, id):
-    self.data = frozenset([frozenset([id])])
+    self.__data = frozenset([frozenset([id])])
 
   def __and__(self, other):
     cnf = Var(1)
-    cnf.data = frozenset.union(self.data, other.data)
-    if cnf.data != frozenset([frozenset([1])]):
-      cnf.data = frozenset.difference(cnf.data, frozenset([frozenset([1])]))
-    if frozenset([-1]) in cnf.data:
-      cnf.data = frozenset([frozenset([-1])])
-    for u in cnf.data:
+    cnf.__data = frozenset.union(self.__data, other.__data)
+    if cnf.__data != frozenset([frozenset([1])]):
+      cnf.__data = frozenset.difference(cnf.__data, frozenset([frozenset([1])]))
+    if frozenset([-1]) in cnf.__data:
+      cnf.__data = frozenset([frozenset([-1])])
+    for u in cnf.__data:
       if len(u) == 1:
         for e in u:
-          for u1 in cnf.data:
+          for u1 in cnf.__data:
             if -e in u1:
-              cnf.data = frozenset([frozenset([-1])])
+              cnf.__data = frozenset([frozenset([-1])])
     return cnf
 
   def __or__(self, other):
     cnf = Var(1)
-    for x in self.data:
-      for y in other.data:
+    for x in self.__data:
+      for y in other.__data:
         u = frozenset.union(x, y)
         for x in u:
           if -x in u:
@@ -33,21 +33,38 @@ class Var:
         if 1 in u:
           u = frozenset([1])
         c = Var(1)
-        c.data = frozenset([u])
+        c.__data = frozenset([u])
         cnf = c & cnf
     return cnf
 
   def __neg__(self):
     k = Var(1)
-    for li in itertools.product(*self.data):
+    for li in itertools.product(*self.__data):
       c = Var(-1)
       for i in li:
-        ci = Var(i)
+        ci = Var(-i)
         c |= ci
       k &= c
     return k
 
+  def print(self):
+    print (self.__data)
+    return ""
+
+  def __str__(self):
+    ret = []
+    for d in self.__data:
+      ret.append(" | ".join(map(str,d)))
+    return "(" + ") & (".join(ret) + ")"
+
+  def __xor__(self, other):
+    """
+    ^
+    """
+    return (self | other) & (-self | -other)
+
+
 if __name__ == "__main__":
-  c = - (Var(-2) & Var(3) & (Var(4) | Var(5)))
-  print(c.data)
+  c = Var(2) ^ Var(3) ^ Var(4)
+  print(c)
 
